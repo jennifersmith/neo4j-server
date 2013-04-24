285e2be8fc44c240b2f02f8bb8db6b6c686c08a2
Jennifer Smith <jennifer@home>
2013-04-24 19:13:21 +0100

    Upgraded to RC1

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.neo4j/neo4j "1.9.M03"]
-                 [org.neo4j.app/neo4j-server "1.9.M03"]
-                 [org.neo4j.app/neo4j-server "1.9.M03" :classifier "static-web"]]
+                 [org.neo4j/neo4j "1.9.RC1"]
+                 [org.neo4j.app/neo4j-server "1.9.RC1"]
+                 [org.neo4j.app/neo4j-server "1.9.RC1" :classifier "static-web"]]
   :repositories [["neo4j-snapshot-repository"
                   { :url "http://m2.neo4j.org/content/repositories/snapshots/"
                    :snapshots true
